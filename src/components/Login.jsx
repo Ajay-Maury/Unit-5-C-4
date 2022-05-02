76485951a9d833870e98a18404666261e9a66a22
Ajay Maurya <97455068+Ajay-Maury@users.noreply.github.com>
2022-05-02 15:33:47 +0530

    Unit -5 c-4

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,27 +1,58 @@
-import { useState } from "react";
-
+import { useContext, useState, useSyncExternalStore } from "react";
+import { AuthContext } from "../context/AuthContext";
+import axios from "axios"
 export const Login = () => {
-  const [uname , setUname] = useState("")
-  const [pass , setPass] = useState("")
+const {isAuth, toggleAuth}=useContext(AuthContext)
+  const [search, setSearch] = useState("");
+  const [password, setPassword] = useState("");
+  async function getData(username,pass) {
+    axios
+      .get(http://localhost:8080/users/?username=${username}&&pass=${pass})
+      .then(function (response) {
+        let userdetails = response.data[0];
+        console.log(userdetails);
+        if (userdetails === undefined)
+          alert("please check the credentails")
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  }
   return (
     <div>
       <input
-        onChange={(e) => setUname(e.target.value)}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
         className="username"
         type="text"
         name="username"
         placeholder="Enter Username"
       />
       <input
-        onChange={(e) => setPass(e.target.value)}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
         className="password"
         type="password"
         name="password"
         placeholder="Enter password"
       />
       {/* On this button click make network req to find user with same username and password */}
-      {/* get his role, if role is `admin` take him to `/orders` page otherwise take him to `/neworder` */}
-      <button className="submit">Login</button>
+      {/* get his role, if role is admin take him to /orders page otherwise take him to /neworder */}
+      <button
+        onClick={() => {
+          console.log(1);
+          getData(search, password);
+        }}
+        className="submit"
+      >
+        Login
+      </button>
     </div>
   );
 };
