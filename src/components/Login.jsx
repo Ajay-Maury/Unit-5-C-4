0b71bd4e66fe234119505eb6f08118b4e3dc778e
Ajay Maurya <97455068+Ajay-Maury@users.noreply.github.com>
2022-05-02 16:19:11 +0530

    Unit -5 c-4

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,34 @@
-import { useContext, useState, useSyncExternalStore } from "react";
-import { AuthContext } from "../context/AuthContext";
+import { useContext, useEffect, useState } from "react";
+// import { AuthContext, } from "../context/AuthContext";
 import axios from "axios"
+import { useNavigate } from "react-router-dom";
 export const Login = () => {
-const {isAuth, toggleAuth}=useContext(AuthContext)
+// const {isAuth, toggleAuth}=useContext(AuthContext )
   const [search, setSearch] = useState("");
   const [password, setPassword] = useState("");
+  const [role, setRole] = useState("")
+
+  let Navigate = useNavigate();
+  
+  // useEffect(() => {
+  //  
+  // }, [getData(search, password)]);
+
   async function getData(username,pass) {
     axios
       .get(`http://localhost:8080/users/?username=${username}&&pass=${pass}`)
       .then(function (response) {
         let userdetails = response.data[0];
-        console.log(userdetails);
         if (userdetails === undefined)
           alert("please check the credentails")
+        console.log(userdetails);
+        setRole(userdetails.role);
+        console.log(role)
+         if (role === "admin") {
+      Navigate(`/orders`);
+    } else if (role === "client") {
+      Navigate("/neworder");
+    }
       })
       .catch(function (error) {
         // handle error
@@ -20,6 +36,8 @@ const {isAuth, toggleAuth}=useContext(AuthContext)
       })
       .then(function () {
         // always executed
+        // console.log(userdetails)
+        // console.log(role)
       });
   }
   return (
@@ -46,7 +64,6 @@ const {isAuth, toggleAuth}=useContext(AuthContext)
       {/* get his role, if role is admin take him to /orders page otherwise take him to /neworder */}
       <button
         onClick={() => {
-          console.log(1);
           getData(search, password);
         }}
         className="submit"
