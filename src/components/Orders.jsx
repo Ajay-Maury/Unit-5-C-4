314e688afa23622d545836c75c1d82e715c23513
Ajay Maurya <97455068+Ajay-Maury@users.noreply.github.com>
2022-05-02 17:00:02 +0530

    Unit -5 c-4

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -3,35 +3,54 @@ import { useEffect, useState } from "react";
 export const Orders = () => {
   //  Get all data when admin logs in and populate it
   // store it in redux
-const [data,setData] = useState([])
+  const [orders, setOrder] = useState([]);
 
-   useEffect(() => {
-    getdata();
-
-    return function cleanup() {};
+  useEffect(() => {
+    getData();
   }, []);
-
-  async function getdata() {
-    const data = await fetch(`http://localhost:8080/orders`).then((d) =>
+  async function getData() {
+    const data = await fetch("http://localhost:8080/orders").then((d) =>
       d.json()
     );
-    setData(data);
-    // console.log(data);
+    setOrder(data);
+    console.log(data);
+  }
+
+  function handleChange(e) {
+    if (e.target.value == "id") {
+      const data = [...orders].sort((a, b) => {
+        return a.id - b.id;
+      });
+      setOrder(data);
+    }
+    if (e.target.value == "status") {
+    }
+    if (e.target.value == "cost") {
+      const data = [...orders].sort((a, b) => {
+        return a.cost - b.cost;
+      });
+      setOrder(data);
+    }
   }
 
   return (
     <div>
       <div>
         <div>
-          <select className="controls" name="progress" id="progress">
+          <select
+            onChange={handleChange}
+            className="controls"
+            name="progress"
+            id="progress"
+          >
             <option value="id">ID</option>
             <option value="status">Status</option>
             <option value="cost">Cost</option>
           </select>
         </div>
-        <table className="orders">
-          <thead>
-            <tr>
+        <table style={{ border: "1px solid black" }} className="orders">
+          <thead style={{ border: "1px solid black" }}>
+            <tr style={{ border: "1px solid black" }}>
               <th>ID</th>
               <th>Problem</th>
               <th>Client Name</th>
@@ -41,31 +60,48 @@ const [data,setData] = useState([])
               <th>Accept</th>
             </tr>
           </thead>
-          <tbody>
-           { data.map((e)=>(
-            <tr className="orders-row">
-              <td className="id">{e.id}</td>
-              <td className="problem">{e.problem}</td>
-              <td className="owner">{e.owner_name}</td>
-              <td className="status">{e.status}</td>
-              <td className="cost">{e.cost}</td>
-              <td className="change-status">
-                {/* Show select dropdown only if status is Not Accepted */}
-                <select className="changeStatus" name="changeStatus">
-                  <option value="Pending">Pending</option>
-                  <option value="In Progress">In Progress</option>
-                  <option value="Done">Done</option>
-                  <option value="Not Accepted">Not Accepted</option>
-                </select>
-              </td>
-              <td className="accept">
-                {/* Show this button only if status is Not Accepted */}
-                {/* on change make request to update it in db, and show changed status in table */}
-                <button>Accept</button>
-              </td>
-            </tr>
-            ))}
-          </tbody>
+          
+          <tbody >
+            {orders.map((e) => (
+              <tr style={{ border: "1px solid black" }} className="orders-row">
+                <td className="id">{e.id}</td>
+                <td className="problem">{e.problem}</td>
+                <td className="owner">{e.owner_name}</td>
+                <td className="status">{e.status}</td>
+                <td className="cost">{e.cost}</td>
+                <td className="change-status">
+                  {/* Show select dropdown only if status is Not Accepted */}
+
+                  {e.status === "Done" ? (
+                    ""
+                  ) : (
+                    <select
+                      className="changeStatus"
+                      name="changeStatus"
+                      onChange={(val) => {
+                        e.status = val.target.value;
+                        console.log(e.status);
+                      }}
+                    >
+                      <option value="Pending">Pending</option>
+                      <option value="In Progress">In Progress</option>
+                      <option value="Done">Done</option>
+                      <option value="Not Accepted">Not Accepted</option>
+                    </select>
+                  )}
+                </td>
+                <td className="accept">
+                  {/* Show this button only if status is Not Accepted */}
+                  {/* on change make request to update it in db, and show changed status in table */}
+                  {e.status !== "Done" && e.cost === undefined ? (
+                    <button>Accept</button>
+                  ) : (
+                    ""
+                  )}
+                </td>
+              </tr>
+          ))}
+            </tbody>
         </table>
       </div>
     </div>
